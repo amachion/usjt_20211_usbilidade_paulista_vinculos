a2809f69885c81cbcbab55348767be5ea1684494
Andreia <andreia.machion@saojudas.br>
2021-04-13 15:48:03 -0300

    finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,26 @@ export class AppComponent {
 
   nome = "Jose";
   idade = 20;
+  esconderCaixa=true;
 
   lancarDados() {
     return Math.floor(Math.random() * 6 + 1);
+  }
+
+  alterarNome(nome) {
+    console.log(nome.target.value);
+    this.nome = nome.target.value;
+  }
+
+  adicionar(nomeInput) {
+    console.log(nomeInput.value);
+    this.esconderCaixa = nomeInput.value.length <= 0;
+    this.nome = nomeInput.value;
+  }
+
+  numero: number;
+  escolher (){
+    this.numero = Math.floor(Math.random() * 100) + 1;
   }
 
 }
